08f1c2bed0bebdecb173090ef29bc99d96aacfe4
박병학(Byeonghak Park) <macosc0625@gmail.com>
2023-02-13 09:50:27 +0900

    Update 여행경로(3)-dfs.py

--- Python/Programmers/bfs_dfs/여행경로(3)-dfs.py
+++ Python/Programmers/bfs_dfs/여행경로(3)-dfs.py
@@ -17,3 +17,31 @@ def solution(tickets):
                 answer.append(dest)
                 tmp_list.append(dest)
     return answer
+
+# 비슷한 개념 활용한 풀이
+# 1. 그래프 생성
+    routes = dict()
+
+    for (start, end) in tickets:
+        routes[start] = routes.get(start, []) + [end]  
+
+    # 2. 시작점 - [끝점] 역순으로 정렬    
+    for r in routes.keys():
+        routes[r].sort(reverse=True)
+
+    # 3. DFS 알고리즘으로 path를 만들어줌.
+    st = ["ICN"]
+    path = []
+    
+    while st:
+        top = st[-1]
+
+        if top not in routes or len(routes[top]) == 0:
+            path.append(st.pop())
+        else:
+            st.append(routes[top][-1])
+            routes[top] = routes[top][:-1]
+    
+    # 4. 만든 path를 거꾸로 돌림.
+    answer = path[::-1]
+    return answer
