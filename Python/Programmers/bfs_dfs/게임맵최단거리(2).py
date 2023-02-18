7867c657e483f0ccdd6b01221fc5848bbddc9d1c
박병학(Byeonghak Park) <macosc0625@gmail.com>
2023-02-18 18:31:02 +0900

    Update 게임맵최단거리(2).py
    효율성 개선

--- Python/Programmers/bfs_dfs/게임맵최단거리(2).py
+++ Python/Programmers/bfs_dfs/게임맵최단거리(2).py
@@ -10,22 +10,26 @@ def solution(maps):
     answer = 0
     y_dir = [-1,1,0,0]
     x_dir = [0,0,-1,1]
-    s_y = 0
-    s_x = 0
-    val_loc = [[i,j] for i in range(len(maps)) for j in range(len(maps[i])) if maps[i][j]==1]
+    y_max = len(maps)
+    x_max = len(maps[0])
     
     que = deque()
-    que.append([s_y,s_x])
+    que.append([0,0,1])
 
-    
     while que:
-        v = que.popleft()
+        dy, dx, v = que.popleft()
         for i in range(len(y_dir)):
-            if [v[0]+y_dir[i], v[1]+x_dir[i]] in val_loc:
-                if maps[v[0]+y_dir[i]][v[1]+x_dir[i]]==1:
-                    maps[v[0]+y_dir[i]][v[1]+x_dir[i]] = maps[v[0]][v[1]]+1
-                    que.append([v[0]+y_dir[i],v[1]+x_dir[i]])
+            ny = dy+y_dir[i]
+            nx = dx+x_dir[i]
+            # 기존 1값이 있는 부분만 찾기위해 리스트를 만들고 해당 리스트에서 찾는 과정이 있었음
+            # 해당 과정으로 인해 시간이 더 걸려 아래와 같이 조건식으로 변경함
+            # 이로 인해 효율성 개선
+            if nx>=0 and nx<x_max and ny>=0 and ny<y_max:
+                if maps[ny][nx]==1:
+                    maps[ny][nx] = v+1
+                    que.append([ny,nx,v+1])
     answer = maps[-1][-1]
+    print(answer)
     if answer == 1 or answer==0:
         return -1
     else:
