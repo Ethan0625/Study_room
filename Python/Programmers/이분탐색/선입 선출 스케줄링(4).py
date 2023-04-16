03292c64f8464094fda81758675f06d3f8fa8422
박병학(Byeonghak Park) <macosc0625@gmail.com>
2023-04-16 20:50:13 +0900

    프로그래머스 Level4 1문제(230416)

---
+++ Python/Programmers/이분탐색/선입 선출 스케줄링(4).py
@@ -0,0 +1,26 @@
+def solution(n, cores):
+    if n <= len(cores):
+        return n
+    else:
+        n -= len(cores)
+        left = 1
+        right = max(cores) * n
+        
+        while left < right:
+            mid = (left + right) // 2
+            capacity = 0
+            for c in cores:
+                capacity += mid // c
+            if capacity >= n:
+                right = mid
+            else:
+                left = mid + 1
+
+        for c in cores:
+            n -= (right-1) // c
+
+        for i in range(len(cores)):
+            if right % cores[i] == 0:
+                n -= 1
+                if n == 0:
+                    return i + 1
